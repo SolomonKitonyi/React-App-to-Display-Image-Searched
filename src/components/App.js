1bf79e86e90269f2d38dc7de330d1713a1102e9c
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-02-03 14:57:07 +0300

    add new react app

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component{
+    onSearchSubmint(term){
+        console.log(term);
+    }
+    render(){
+        return (
+            <div className = "ui container" style = {{marginTop:'10px'}}>
+                <SearchBar onSubmit = {this.onSearchSubmint}/>
+            </div>
+        );
+    };
+}
+
+export default App;
